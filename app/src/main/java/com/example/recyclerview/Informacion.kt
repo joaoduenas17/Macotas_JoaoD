04bd9018951cc6f5a5d84da3c2fe711788553f71
joaoduenas17 <joaoduenasm@hotmail.com>
2022-11-21 23:10:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Informacion.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class Informacion(val nombre:String, val edad:Int, val raza:String, val imagen:String)
---
+++ app/src/main/java/com/example/recyclerview/InformacionProvider.kt
@@ -0,0 +1,19 @@
+package com.example.recyclerview
+
+class InformacionProvider {
+    companion object{
+        val info = listOf<Informacion>(
+            Informacion("Max",3,"Dalmata","https://www.mundoperro.net/wp-content/uploads/dalmata-1200x900.jpg"),
+            Informacion("Skyler",4,"   Husky","https://es.bellfor.info/image/catalog/Blog/blog_pictures_multilang/husky-merkmale.jpg"),
+            Informacion("Alice",1,"    Pug","https://soyunperro.com/wp-content/uploads/2019/08/perro-pug-en-el-jardin.jpg"),
+            Informacion("Juan",6,"Jack Russell","https://media.istockphoto.com/photos/funny-guilty-confused-dog-picture-id1187197997?b=1&k=6&m=1187197997&s=170667a&w=0&h=uKUNE1UbseLU9LL-ZzU59vkvBKEil-aI6fm8MbNoj0E="),
+            Informacion("Chop",10," Rottweiler","https://t2.ea.ltmcdn.com/es/posts/4/8/7/alimentacion_de_un_rottweiler_cachorro_adulto_y_anciano_23784_600_square.jpg"),
+            Informacion("Thor",7,"Pastor Aleman","https://t2.ea.ltmcdn.com/es/posts/3/2/9/curiosidades_del_pastor_aleman_23923_600_square.jpg"),
+            Informacion("Goliat",5,"  Bulldog","https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=400,height=400,fit=cover/animal/breed/pictures/61a8ad46965d5085827792.jpg"),
+            Informacion("Coco",8,"San Bernardo","https://t1.ea.ltmcdn.com/es/razas/1/6/1/san-bernardo_161_0_orig.jpg"),
+            Informacion("Yuki",9,"Chihuahua","https://www.purina.es/sites/default/files/2021-02/BREED%20Hero_0034_chihuahua_smooth.jpg"),
+            Informacion("Rocky",2,"   Boxer","https://t1.uc.ltmcdn.com/es/posts/0/3/4/como_adiestrar_a_un_boxer_34430_600_square.jpg"))
+
+    }
+
+}
